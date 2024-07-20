0eff7c7afbd39464f4af8298418a2a77877dc77f
kali8271 <akalimullah900@gmail.com>
2024-07-20 13:13:49 +0530

    updated

--- init_setup.sh
+++ init_setup.sh
@@ -2,11 +2,11 @@ echo [$(date)]: "START"
 
 echo [$(date)]: "creating env with python 3.8 version" 
 
-conda create --prefix ./env python=3.8 -y
+# conda create --prefix ./env python=3.8 -y
 
 echo [$(date)]: "activating the environment"
 
-source activate ./env
+source activate ./envs
 
 echo [$(date)]: "installing the dev requirements"
 
